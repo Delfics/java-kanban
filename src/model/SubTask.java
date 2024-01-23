f42eb0ee8b5778dc4553c87ea0cc1b34cd299fba
Delfics <soulspirit95@gmail.com>
2024-01-23 23:56:45 +0300

    Коммит номер 4 , доработал комментарии по ТЗ 4

---
+++ src/model/SubTask.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.util.Objects;
+
+public class SubTask extends Task {
+    private final int epicId;
+
+    public SubTask(int id, String name, String description, int epicId) {
+        super(id, name, description);
+        this.epicId = epicId;
+
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
+
+    @Override
+    public String toString() {
+        return "Идентификатор " + super.getId() + "\nТип: Подзадача " + "\nСтатус: " + super.getStatus()
+                + "\nНазвание: " + super.getName() + "\n" + super.getDescription() + "\n";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        } else if (o == null) {
+            return false;
+        } else if (this.getClass() != o.getClass()) {
+            return false;
+        } else if (!super.equals(o)) {
+            return false;
+        }
+        SubTask otherSubtask = (SubTask) o;
+        return (epicId == otherSubtask.epicId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(epicId);
+    }
+}
